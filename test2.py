0e7b7e40c0ca80cf87d3fe0b913a4e08fd5d9a9c
RuojiWang <1035456235@qq.com>
2019-05-01 23:46:30 +0800

    Add files via upload

--- test2.py
+++ test2.py
@@ -83,13 +83,32 @@ def load_best_model(title_and_nodes):
 #lr相关的部分咯
 def lr_f(params):
     
-    clf = LogisticRegression(penalty=params["penalty"], C=params["C"],
+    print("title", params["title"])
+    print("path", params["path"])
+    print("mean", params["mean"])
+    print("std", params["std"])
+    print("feature_num", params["feature_num"])
+    print("penalty", params["penalty"]) 
+    print("C", params["C"])
+    print("fit_intercept", params["fit_intercept"])
+    print("class_weight", params["class_weight"])
+
+    #设置一个random_state值以防分类器初始参数有些差异
+    #但是好像据说sklearn的逻辑回归模型这个设置是无效的
+    #怪不得我看别人的代码都会设置这个东西，原来是这个意思呀
+    clf = LogisticRegression(penalty=params["penalty"], 
+                             C=params["C"],
                              fit_intercept=params["fit_intercept"],
-                             class_weight=params["class_weight"])
+                             class_weight=params["class_weight"],
+                             random_state=42)
+    
     selector = RFE(clf, n_features_to_select=params["feature_num"])
     X_train_scaled_new = selector.fit_transform(X_train_scaled, Y_train)
-    skf = StratifiedKFold(Y_train, n_folds=10, shuffle=True, random_state=None)
+    skf = StratifiedKFold(Y_train, n_folds=10, shuffle=True, random_state=42)
     metric = cross_val_score(clf, X_train_scaled_new, Y_train, cv=skf, scoring="roc_auc").mean()
+    
+    print(metric)
+    print()
     return -metric
     
 def parse_lr_nodes(trials, space_nodes):
@@ -113,9 +132,42 @@ def parse_lr_nodes(trials, space_nodes):
     
     return best_nodes
 
-def train_lr_model(nodes, X_train_scaled, Y_train):
+#几乎所有传统模型的random_state设置了具体的值，且超参一样那么结果完全一样
+#如果设置的是None则每次的训练结果有所差异，这就是为什么那些代码都会设置random_state
+def train_lr_model(best_nodes, X_train_scaled, Y_train):
     
-    return 
+    clf = LogisticRegression(penalty=best_nodes["penalty"], 
+                             C=best_nodes["C"],
+                             fit_intercept=best_nodes["fit_intercept"],
+                             class_weight=best_nodes["class_weight"],
+                             random_state=42)
+    
+    selector = RFE(clf, n_features_to_select=best_nodes["feature_num"])
+    X_train_scaled_new = selector.fit_transform(X_train_scaled, Y_train)
+    
+    clf.fit(X_train_scaled_new, Y_train)
+    print(clf.score(X_train_scaled_new, Y_train))
+    
+    return clf, X_train_scaled_new
+
+#这个主要是为了判断训练多次出来的逻辑回归模型是否完全一致
+#判断逻辑回归模型完全一致的办法就是他们的截距和系数完全一致吧
+#还记得模型训练过程其实是梯度下降算法，所以即便是所有参数以及超参相同
+#训练数据的顺序改变也会导致最终训练出来的模型有所差异吧（这就是梯度下降算法）。
+#可能这个真的是玄学很难判断同样的超参但是shuffle之后的数据训练出的模型谁更好吧？
+#最后的实验结果证明train_lr_model中clf的random_state为常数时候训练出的所有模型一样
+#设置为None的时候每次都不一样，这是因为训练数据的顺序被改变了，梯度下降算法就是这样的
+def compare_lr_model(clf1, clf2):
+
+    #print(type(clf1.coef_))
+    #print(type(clf1.intercept_))
+    #print(type(clf1.n_iter_))
+    #print(type(clf1.get_params()))
+    if((clf1.coef_==clf2.coef_).all() and (clf1.intercept_==clf2.intercept_).all() 
+       and (clf1.n_iter_==clf2.n_iter_).all()):
+        return True
+    else:
+        return False
 
 lr_space = {"title":hp.choice("title", ["stacked_don't_overfit!_II"]),
             "path":hp.choice("path", ["Don't_Overfit!_II_Prediction.csv"]),
@@ -124,7 +176,7 @@ lr_space = {"title":hp.choice("title", ["stacked_don't_overfit!_II"]),
             #这个linspace返回的类型是ndarray类型的数据
             "feature_num":hp.choice("feature_num", np.linspace(1,300,300)),
             "penalty":hp.choice("penalty", ["l1", "l2"]),
-            "C":hp.choice("C", np.logspace(-3, 5, 100)),
+            "C":hp.choice("C", np.logspace(-3, 5, 40)),
             "fit_intercept":hp.choice("fit_intercept", ["True", "False"]),
             "class_weight":hp.choice("class_weight", ["balanced", None])
             }
@@ -135,12 +187,101 @@ lr_space_nodes = {"title":["stacked_don't_overfit!_II"],
                   "std":[0],
                   "feature_num":np.linspace(1,300,300),
                   "penalty":["l1", "l2"],
-                  "C":np.logspace(-3, 5, 100),
+                  "C":np.logspace(-3, 5, 40),
                   "fit_intercept":["True", "False"],
                   "class_weight":["balanced", None]
                  }
 
+"""
+trials = Trials()
+algo = partial(tpe.suggest, n_startup_jobs=10)
+best_params = fmin(lr_f, lr_space, algo=tpe.suggest, max_evals=1, trials=trials)
+best_nodes = parse_lr_nodes(trials, lr_space_nodes)
+save_inter_params(trials, lr_space_nodes, best_nodes, "lr_don't_overfit!_II")
+train_lr_model(best_nodes, X_train_scaled, Y_train)
+"""
+
+"""
+trials = Trials()
+algo = partial(tpe.suggest, n_startup_jobs=10)
+best_params = fmin(lr_f, lr_space, algo=tpe.suggest, max_evals=1, trials=trials)
+best_nodes = parse_lr_nodes(trials, lr_space_nodes)
+save_inter_params(trials, lr_space_nodes, best_nodes, "lr_don't_overfit!_II")
+clf1, X_train_scaled1 = train_lr_model(best_nodes, X_train_scaled, Y_train)
+clf2, X_train_scaled2 = train_lr_model(best_nodes, X_train_scaled, Y_train)
+if(compare_lr_model(clf1, clf2)):
+    print("1")
+else:
+    print("2")
+"""
+
 #xgboost相关的部分咯
+#我还是有点担心这里面的函数输出的是错误的值，所以函数print一下吧
+def xgb_f(params):
+    
+    print("title", params["title"])
+    print("path", params["path"])
+    print("mean", params["mean"])
+    print("std", params["std"])
+    print("feature_num", params["feature_num"])
+    print("gamma", params["gamma"]) 
+    print("max_depth", params["max_depth"])
+    print("learning_rate", params["learning_rate"])
+    print("min_child_weight", params["min_child_weight"])
+    print("subsample", params["subsample"])
+    print("colsample_bytree", params["colsample_bytree"])
+    print("reg_alpha", params["reg_alpha"])
+    print("reg_lambda", params["reg_lambda"])
+    print("n_estimators", params["n_estimators"])
+
+    clf = XGBClassifier(gamma=params["gamma"],
+                        max_depth=params["max_depth"],
+                        learning_rate=params["learning_rate"],
+                        min_child_weight=params["min_child_weight"],
+                        subsample=params["subsample"],
+                        colsample_bytree=params["colsample_bytree"],
+                        reg_alpha=params["reg_alpha"],
+                        reg_lambda=params["reg_lambda"],
+                        n_estimators=int(params["n_estimators"]),
+                        random_state=42)
+
+    clf.fit(X_train_scaled, Y_train)
+    
+    selector = RFE(clf, n_features_to_select=params["feature_num"])
+    X_train_scaled_new = selector.fit_transform(X_train_scaled, Y_train)
+    skf = StratifiedKFold(Y_train, n_folds=10, shuffle=True, random_state=42)
+    metric = cross_val_score(clf, X_train_scaled_new, Y_train, cv=skf, scoring="roc_auc").mean()
+    
+    print(metric)
+    print()
+    return -metric
+    
+def parse_xgb_nodes(trials, space_nodes):
+    
+    trials_list =[]
+    for item in trials.trials:
+        trials_list.append(item)
+    trials_list.sort(key=lambda item: item['result']['loss'])
+    
+    best_nodes = {}
+    best_nodes["title"] = space_nodes["title"][trials_list[0]["misc"]["vals"]["title"][0]]
+    best_nodes["path"] = space_nodes["path"][trials_list[0]["misc"]["vals"]["path"][0]]
+    best_nodes["mean"] = space_nodes["mean"][trials_list[0]["misc"]["vals"]["mean"][0]]
+    best_nodes["std"] = space_nodes["std"][trials_list[0]["misc"]["vals"]["std"][0]]
+    
+    best_nodes["feature_num"] = space_nodes["feature_num"][trials_list[0]["misc"]["vals"]["feature_num"][0]]
+    best_nodes["gamma"] = space_nodes["gamma"][trials_list[0]["misc"]["vals"]["gamma"][0]]
+    best_nodes["max_depth"] = space_nodes["max_depth"][trials_list[0]["misc"]["vals"]["max_depth"][0]]
+    best_nodes["learning_rate"] = space_nodes["learning_rate"][trials_list[0]["misc"]["vals"]["learning_rate"][0]]
+    best_nodes["min_child_weight"] = space_nodes["min_child_weight"][trials_list[0]["misc"]["vals"]["min_child_weight"][0]]
+    best_nodes["subsample"] = space_nodes["subsample"][trials_list[0]["misc"]["vals"]["subsample"][0]]
+    best_nodes["colsample_bytree"] = space_nodes["colsample_bytree"][trials_list[0]["misc"]["vals"]["colsample_bytree"][0]]
+    best_nodes["reg_alpha"] = space_nodes["reg_alpha"][trials_list[0]["misc"]["vals"]["reg_alpha"][0]]
+    best_nodes["reg_lambda"] = space_nodes["reg_lambda"][trials_list[0]["misc"]["vals"]["reg_lambda"][0]]
+    best_nodes["n_estimators"] = space_nodes["n_estimators"][trials_list[0]["misc"]["vals"]["n_estimators"][0]]
+
+    return best_nodes
+
 xgb_space = {"title":hp.choice("title", ["stacked_don't_overfit!_II"]),
              "path":hp.choice("path", ["Don't_Overfit!_II_Prediction.csv"]),
              "mean":hp.choice("mean", [0]),
@@ -148,27 +289,57 @@ xgb_space = {"title":hp.choice("title", ["stacked_don't_overfit!_II"]),
              "feature_num":hp.choice("feature_num", np.linspace(1,300,300)),
              "gamma":hp.choice("gamma", [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.3, 0.5, 0.7, 0.9]),
              "max_depth":hp.choice("max_depth", [3, 5, 7, 9, 12, 15, 17, 25]),
-             "learning_rate":hp.choice("learning_rate", np.linspace(0.01, 1.00, 100)),
+             "learning_rate":hp.choice("learning_rate", np.linspace(0.01, 0.50, 50)),
              "min_child_weight":hp.choice("min_child_weight", [1, 3, 5, 7, 9]),
              "subsample":hp.choice("subsample", [0.6, 0.7, 0.8, 0.9, 1.0]),
              "colsample_bytree":hp.choice("colsample_bytree", [0.6, 0.7, 0.8, 0.9, 1.0]),
              "reg_alpha":hp.choice("reg_alpha", [0.0, 0.1, 0.5, 1.0]),
              "reg_lambda":hp.choice("reg_lambda", [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 1.0]),
-             "n_estimators":hp.choice("n_estimators", np.linspace(50, 500, 45)),
+             "n_estimators":hp.choice("n_estimators", np.linspace(50, 500, 46))
              }
 
 xgb_space_nodes = {"title":["stacked_don't_overfit!_II"],
                    "path":["Don't_Overfit!_II_Prediction.csv"],
                    "mean":[0],
                    "std":[0],
-                  }
+                   "feature_num":np.linspace(1,300,300),
+                   "gamma":[0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.3, 0.5, 0.7, 0.9],
+                   "max_depth":[3, 5, 7, 9, 12, 15, 17, 25],
+                   "learning_rate":np.linspace(0.01, 0.50, 50),
+                   "min_child_weight":[1, 3, 5, 7, 9],
+                   "subsample":[0.6, 0.7, 0.8, 0.9, 1.0],
+                   "colsample_bytree":[0.6, 0.7, 0.8, 0.9, 1.0],
+                   "reg_alpha":[0.0, 0.1, 0.5, 1.0],
+                   "reg_lambda":[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 1.0],
+                   "n_estimators":np.linspace(50, 500, 46)
+                   }
+
+def train_xgb_model(best_nodes, X_train_scaled, Y_train):
+    
+    clf = XGBClassifier(gamma=best_nodes["gamma"],
+                        max_depth=best_nodes["max_depth"],
+                        learning_rate=best_nodes["learning_rate"],
+                        min_child_weight=best_nodes["min_child_weight"],
+                        subsample=best_nodes["subsample"],
+                        colsample_bytree=best_nodes["colsample_bytree"],
+                        reg_alpha=best_nodes["reg_alpha"],
+                        reg_lambda=best_nodes["reg_lambda"],
+                        n_estimators=int(best_nodes["n_estimators"]),
+                        random_state=42)
+    
+    selector = RFE(clf, n_features_to_select=best_nodes["feature_num"])
+    X_train_scaled_new = selector.fit_transform(X_train_scaled, Y_train)
+    
+    clf.fit(X_train_scaled_new, Y_train)
+    print(clf.score(X_train_scaled_new, Y_train))
+    
+    return clf, X_train_scaled_new
 
 """
 trials = Trials()
 algo = partial(tpe.suggest, n_startup_jobs=10)
-best = fmin(lr_f, lr_space, algo=tpe.suggest, max_evals=8000)
-best_nodes = parse_lr_nodes(trials, lr_space_nodes)
-save_inter_params(trials, lr_space_nodes, best_nodes, "lr_don't_overfit!_II")
+best_params = fmin(xgb_f, xgb_space, algo=tpe.suggest, max_evals=1, trials=trials)
+best_nodes = parse_xgb_nodes(trials, xgb_space_nodes)
+save_inter_params(trials, xgb_space_nodes, best_nodes, "xgb_don't_overfit!_II")
+train_xgb_model(best_nodes, X_train_scaled, Y_train)
 """
-
-gxb = XGBClassifier()
